1b0b7cf22bafa83dbac7b5af73105e2f2bda79a4
Nguyen_Le_Hoang_Khai <nkha8283@gmail.com>
2025-11-15 15:45:01 +0700

    Update Bai_01.py

--- Bai_03/Bai_01.py
+++ Bai_03/Bai_01.py
@@ -5,4 +5,4 @@ chia_het_cho_3 = (n % 3 == 0)
 if la_chan and lon_hon_10 and chia_het_cho_3:
     print("Số thỏa mãn cả 3 điều kiện")
 else:
-    print("Trượt")
+    print("Không thỏa mãn cả 3 điều kiện!!!")
