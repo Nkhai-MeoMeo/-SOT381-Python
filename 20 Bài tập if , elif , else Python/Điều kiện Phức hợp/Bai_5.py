e0aa0fda9e3cea0d812524df32f2becbbb51633e
Nguyen_Le_Hoang_Khai <nkha8283@gmail.com>
2025-11-18 19:24:31 +0700

    Update Bai_5.py

--- 20 Bài tập if , elif , else Python/Điều kiện Phức hợp/Bai_5.py
+++ 20 Bài tập if , elif , else Python/Điều kiện Phức hợp/Bai_5.py
@@ -1,14 +1,1 @@
-chu = input("Nhập 1 ký tự để kiểm tra : ")
 
-ki_tu = "a, e, i, o, u"
-
-if len(chu) != 1:
-    print("Vui lòng nhập 1 ký tự : ")
-else:
-    ch_lower = chu.lower()
-    vowels = ['a', 'e', 'i', 'o', 'u']
-
-    if ch_lower in vowels:
-        print("Nguyên âm")
-    else:
-        print("không nguyên âm")
